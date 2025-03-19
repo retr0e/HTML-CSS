832ad41105eafc80f658c36f9e30b13d4e4a57b2
Dawid Śleziak <dawid.sleziak@gmail.com>
2025-03-19 14:24:21 +0100

    Finish art gallery project

--- CSS/P5_ProjectFive_ArtGallery/app.js
+++ CSS/P5_ProjectFive_ArtGallery/app.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobileNav');
 
 hamburger.addEventListener('click', () => {
   hamburger.querySelector('.bar1').classList.toggle('animateBar1');
   hamburger.querySelector('.bar2').classList.toggle('animateBar2');
   hamburger.querySelector('.bar3').classList.toggle('animateBar3');
+  mobileNav.classList.toggle('openDrawer');
 });
